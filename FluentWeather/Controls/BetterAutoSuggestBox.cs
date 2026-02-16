cd60eb78df9c107e201eb8c1454a2ecf0bb4e9b4
Gabriele <gabboxl0@gmail.com>
2026-02-16 17:56:48 +0100

    Remove unused code

--- FluentWeather/Controls/BetterAutoSuggestBox.cs
+++ FluentWeather/Controls/BetterAutoSuggestBox.cs
@@ -23,97 +23,6 @@ namespace FluentWeather.Controls
             NetworkError
         }
 
-        /*
-        // COM shit stolen from https://github.com/hexawyz/Exo/commit/e23ac45a9684f4d3f22d9a8947c0cda68f0bf93c
-        public static bool TryAs<TInterface>(object? value, [NotNullWhen(true)] out TInterface? result)
-		where TInterface : class
-	    {
-		    if (value is null) goto NotSuccessful;
-
-		    if (value is TInterface i)
-		    {
-			    result = i;
-			    return true;
-		    }
-
-		    ObjectReference<IUnknownVftbl> unknown;
-
-		    if (ComWrappersSupport.TryUnwrapObject(value, out var objRef))
-		    {
-			    unknown = objRef.As<IUnknownVftbl>(IID.IID_IUnknown);
-		    }
-		    else if (value is IWinRTObject winRTObject)
-		    {
-			    unknown = winRTObject.NativeObject.As<IUnknownVftbl>(IID.IID_IUnknown);
-		    }
-		    else
-		    {
-			    goto NotSuccessful;
-		    }
-
-		    using (unknown)
-		    {
-			    if (typeof(TInterface).IsDefined(typeof(ComImportAttribute)))
-			    {
-				    Guid iid = typeof(TInterface).GUID;
-				    var qir = Marshal.QueryInterface(unknown.ThisPtr, in iid, out var ppv);
-				    if (qir >= 0)
-				    {
-					    try
-					    {
-						    result = (TInterface)Marshal.GetObjectForIUnknown(ppv);
-					    }
-					    finally
-					    {
-						    Marshal.Release(ppv);
-					    }
-				    }
-				    else if ((uint)qir == 0x80004002U)
-				    {
-					    goto NotSuccessful;
-				    }
-				    else
-				    {
-					    Marshal.ThrowExceptionForHR(qir);
-				    }
-			    }
-		    }
-	    NotSuccessful:;
-		    result = null;
-		    return false;
-	    }
-
-	    private static TElement? FindChild<TElement>(FrameworkElement element, string name)
-		    where TElement : FrameworkElement
-	    {
-		    int childCount = VisualTreeHelper.GetChildrenCount(element);
-		    if (childCount > 0)
-		    {
-			    return FindChild<TElement>(element, childCount, name);
-		    }
-		    return null;
-	    }
-
-	    private static TElement? FindChild<TElement>(FrameworkElement element, int childCount, string name)
-		    where TElement : FrameworkElement
-	    {
-		    for (int i = 0; i < childCount; i++)
-		    {
-			    if (VisualTreeHelper.GetChild(element, i) is not FrameworkElement child) continue;
-			    if (child.Name == name && TryAs<TElement>(child, out var typedChild)) return typedChild;
-			    int count = VisualTreeHelper.GetChildrenCount(child);
-			    if (count > 0 && FindChild<TElement>(child, count, name) is { } childChild) return childChild;
-		    }
-		    return null;
-	    }
-
-        // example usage
-        //if(!TryAs<Border>(popups[0].Child, out var border)) return;
-        //if(!TryAs<ListView>(border.Child, out var listView)) return;
-        //if(!TryAs<StackPanel>((IWinRTObject)listView.Header, out var stack)) return;
-
-        */
-
         public static void ShowHideCustomHeader(this AutoSuggestBox mySealedClass, bool show, AutoSuggestBoxHeaderType type = AutoSuggestBoxHeaderType.Loading)
         {
             var popups = new List<Popup>();
@@ -125,7 +34,7 @@ namespace FluentWeather.Controls
                 return;
             }
 
-            // More info on WinRT As() method and why it is needed for AOT instead of a language cast here:
+            // More info on WinRT As() extension method and why it is needed for AOT instead of a language cast here:
             // https://github.com/microsoft/microsoft-ui-xaml/issues/10060
             //
             // Native AOT compilation's trimming removes all types information so we need to fallback querying COM interfaces directly
