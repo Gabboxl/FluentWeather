4b5c241b39a7ba3c70f5effb0e24f4e29c2d9b15
Gabriele <gabboxl0@gmail.com>
2026-02-16 17:56:30 +0100

    Fix UI tree reflection code for NativeAOT compilation

--- FluentWeather/Controls/BetterAutoSuggestBox.cs
+++ FluentWeather/Controls/BetterAutoSuggestBox.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using FluentWeather.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using FluentWeather.Helpers;
+using Windows.UI.Xaml.Media;
+using WinRT;
+using WinRT.Interop;
 
 namespace FluentWeather.Controls
 {
@@ -16,7 +23,97 @@ namespace FluentWeather.Controls
             NetworkError
         }
 
-        // Extension method to double the Value property
+        /*
+        // COM shit stolen from https://github.com/hexawyz/Exo/commit/e23ac45a9684f4d3f22d9a8947c0cda68f0bf93c
+        public static bool TryAs<TInterface>(object? value, [NotNullWhen(true)] out TInterface? result)
+		where TInterface : class
+	    {
+		    if (value is null) goto NotSuccessful;
+
+		    if (value is TInterface i)
+		    {
+			    result = i;
+			    return true;
+		    }
+
+		    ObjectReference<IUnknownVftbl> unknown;
+
+		    if (ComWrappersSupport.TryUnwrapObject(value, out var objRef))
+		    {
+			    unknown = objRef.As<IUnknownVftbl>(IID.IID_IUnknown);
+		    }
+		    else if (value is IWinRTObject winRTObject)
+		    {
+			    unknown = winRTObject.NativeObject.As<IUnknownVftbl>(IID.IID_IUnknown);
+		    }
+		    else
+		    {
+			    goto NotSuccessful;
+		    }
+
+		    using (unknown)
+		    {
+			    if (typeof(TInterface).IsDefined(typeof(ComImportAttribute)))
+			    {
+				    Guid iid = typeof(TInterface).GUID;
+				    var qir = Marshal.QueryInterface(unknown.ThisPtr, in iid, out var ppv);
+				    if (qir >= 0)
+				    {
+					    try
+					    {
+						    result = (TInterface)Marshal.GetObjectForIUnknown(ppv);
+					    }
+					    finally
+					    {
+						    Marshal.Release(ppv);
+					    }
+				    }
+				    else if ((uint)qir == 0x80004002U)
+				    {
+					    goto NotSuccessful;
+				    }
+				    else
+				    {
+					    Marshal.ThrowExceptionForHR(qir);
+				    }
+			    }
+		    }
+	    NotSuccessful:;
+		    result = null;
+		    return false;
+	    }
+
+	    private static TElement? FindChild<TElement>(FrameworkElement element, string name)
+		    where TElement : FrameworkElement
+	    {
+		    int childCount = VisualTreeHelper.GetChildrenCount(element);
+		    if (childCount > 0)
+		    {
+			    return FindChild<TElement>(element, childCount, name);
+		    }
+		    return null;
+	    }
+
+	    private static TElement? FindChild<TElement>(FrameworkElement element, int childCount, string name)
+		    where TElement : FrameworkElement
+	    {
+		    for (int i = 0; i < childCount; i++)
+		    {
+			    if (VisualTreeHelper.GetChild(element, i) is not FrameworkElement child) continue;
+			    if (child.Name == name && TryAs<TElement>(child, out var typedChild)) return typedChild;
+			    int count = VisualTreeHelper.GetChildrenCount(child);
+			    if (count > 0 && FindChild<TElement>(child, count, name) is { } childChild) return childChild;
+		    }
+		    return null;
+	    }
+
+        // example usage
+        //if(!TryAs<Border>(popups[0].Child, out var border)) return;
+        //if(!TryAs<ListView>(border.Child, out var listView)) return;
+        //if(!TryAs<StackPanel>((IWinRTObject)listView.Header, out var stack)) return;
+
+        */
+
         public static void ShowHideCustomHeader(this AutoSuggestBox mySealedClass, bool show, AutoSuggestBoxHeaderType type = AutoSuggestBoxHeaderType.Loading)
         {
             var popups = new List<Popup>();
@@ -28,30 +125,32 @@ namespace FluentWeather.Controls
                 return;
             }
 
-            Grid element = null;
-            var stack = ((popups[0].Child as Border).Child as ListView).Header as StackPanel;
+            // More info on WinRT As() method and why it is needed for AOT instead of a language cast here:
+            // https://github.com/microsoft/microsoft-ui-xaml/issues/10060
+            //
+            // Native AOT compilation's trimming removes all types information so we need to fallback querying COM interfaces directly
+            // and then wrapping them back into WinRT objects, which is what As() does under the hood.
+            var border = popups[0].Child.As<Border>();
+            var listView = border.Child.As<ListView>();
+            var stack = listView.Header.As<StackPanel>();
+
+            Grid element;
 
             switch (type)
             {
                 case AutoSuggestBoxHeaderType.Loading:
-                    element = stack.Children[0] as Grid;
+                    element = (Grid)(stack.Children[0]);
                     break;
 
                 case AutoSuggestBoxHeaderType.NetworkError:
-                    element = stack.Children[1] as Grid;
+                    element = (Grid)(stack.Children[1]);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type), type, null);
             }
 
-            //    element.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
-
-            //check if any of stack children is visible
-            //if so, hide it
-            
-            //(grid.Children[1] as TextBlock).Text = "My custom text";
             element.Visibility = show ? Visibility.Visible : Visibility.Collapsed;
-
+            
             int visibleCount = 0;
             foreach (var child in stack.Children)
             {
@@ -59,11 +158,11 @@ namespace FluentWeather.Controls
                 {
                     if (grid.Visibility == Visibility.Visible)
                     {
-                       // grid.Visibility = Visibility.Collapsed;
                         visibleCount++;
                     }
                 }
             }
+
             stack.Height = visibleCount > 0 ? 50 : 0;
         }
     }
