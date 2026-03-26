31532f1c1103c067bbb2b737d6c553b75dcb71a2
Gabriele <gabboxl0@gmail.com>
2026-03-26 20:18:36 +0100

    Minor fix

--- FluentWeather/Services/FirstRunDisplayService.cs
+++ FluentWeather/Services/FirstRunDisplayService.cs
@@ -20,7 +20,7 @@ namespace FluentWeather.Services
                 return;
 
             //set is24HourFormat settings based on system settings (values can be "24HourClock" or "12HourClock")
-            string systemClockType = Windows.System.UserProfile.GlobalizationPreferences.Clocks.FirstOrDefault();
+            var systemClockType = Windows.System.UserProfile.GlobalizationPreferences.Clocks.FirstOrDefault();
 
             await ApplicationData.Current.LocalSettings.SaveAsync("is12HourFormat", systemClockType == "12HourClock");
 
