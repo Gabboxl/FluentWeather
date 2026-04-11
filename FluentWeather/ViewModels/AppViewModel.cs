7216329a574028d44dd08d98ba9d101562893c48
Gabriele <gabboxl0@gmail.com>
2026-04-11 13:20:55 +0200

    Minor fix

--- FluentWeather/ViewModels/AppViewModel.cs
+++ FluentWeather/ViewModels/AppViewModel.cs
@@ -8,7 +8,7 @@ namespace FluentWeather.ViewModels
         //public delegate void RequestUiUpdateDelegate();
         //public event RequestUiUpdateDelegate RequestUiUpdate;
 
-        public event Action UpdateUiAction;
+        public event Action? UpdateUiAction;
 
         //https://stackoverflow.com/a/4378380/9008381
         public void UpdateUi()
